0f247db2a73c7a4467522b809e36bb89108a0676
Nicholas H.R. Sims <nickhrsims@gmail.com>
2025-03-14 08:56:32 -0400

    feat: AABB base impl

---
+++ src/axis-aligned-bounding-box.ts
@@ -0,0 +1,80 @@
+import { Vector2 } from './vector2';
+
+const radius = (diameter: number): number => diameter / 2;
+
+export type AxisAlignedBoundingBoxParams = {
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+}
+
+export class AxisAlignedBoundingBox {
+  private x: number;
+  private y: number;
+  private w: number;
+  private h: number;
+
+  private constructor(params: AxisAlignedBoundingBoxParams) {
+    const { x, y, w, h } = params;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+
+  static create(params: AxisAlignedBoundingBoxParams) {
+    return new AxisAlignedBoundingBox(params);
+  }
+
+  draw(): void {
+    const { x, y, w, h } = this;
+    rect(x, y, w, h);
+  }
+
+  get position(): Vector2 {
+    const { x, y } = this;
+    return [x, y];
+  }
+
+  set position(vec2: Vector2) {
+    const [x, y] = vec2;
+    this.x = x;
+    this.y = y;
+  }
+
+  get center(): Vector2 {
+    const { x, y, w, h } = this;
+    return [
+      x + radius(w),
+      y + radius(h),
+    ];
+  }
+
+  set center(vec2: Vector2) {
+    const [x, y] = vec2;
+    const { w, h } = this;
+    this.x = x - radius(w);
+    this.y = y - radius(h);
+  }
+
+  get top(): number {
+    return this.y;
+  }
+
+  get left(): number {
+    return this.x;
+  }
+
+  get right(): number {
+    return this.x + this.w;
+  }
+
+  get bottom(): number {
+    return this.y + this.h;
+  }
+
+  isBoundedWithin(parent: AxisAlignedBoundingBox): boolean {
+    return parent.left < this.left && this.right < parent.right && parent.top < this.top && this.bottom < parent.bottom;
+  }
+}
