9e51666e954a24d2111236eecfb3901fcd71aa4c
Nicholas H.R. Sims <nickhrsims@gmail.com>
2025-03-18 11:39:30 -0400

    refactor: output to sketch.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from '@rollup/plugin-typescript';
 export default {
   input: 'src/main.ts',
   output: {
-    file: 'dist/bundle.js',
+    file: 'dist/sketch.js',
     format: 'iife', // Immediately Invoked Function Expression (compatible with p5.js)
     name: 'p5Sketch',
     sourcemap: false, // Keep it false for cleaner output
