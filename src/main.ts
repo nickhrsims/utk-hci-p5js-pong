8b60f7005fc822d4bf86b0f7adeab4bdf0cbc7cb
Nicholas H.R. Sims <nickhrsims@gmail.com>
2025-03-19 15:11:23 -0400

    feat: configuration options GUI

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { Game } from "./game";
+import { Game, GameConfig } from "./game";
 
-const game = new Game({
+const gameConfig: GameConfig = {
   score: {
     textSize: 24,
     limit: 7,
@@ -32,18 +32,90 @@ const game = new Game({
       down: 77, // M
     }
   }
-});
+};
 
+let game: Game = new Game(gameConfig);
+
+const newGame = (): void => {
+  game = new Game(gameConfig);
+}
+
+const setBallSizeTiny = (): void => {
+  gameConfig.ball.radius = 2;
+  newGame();
+}
+
+const setBallSizeNormal = (): void => {
+  gameConfig.ball.radius = 6;
+  newGame();
+}
+
+const setBallSizeLarge = (): void => {
+  gameConfig.ball.radius = 10;
+  newGame();
+}
+
+const setBallSizeComical = (): void => {
+  gameConfig.ball.radius = 20;
+  newGame();
+}
+
+const setPaddleSpeedNormal = (): void => {
+  gameConfig.paddles.speed = 0.25;
+  newGame();
+}
+
+const setPaddleSpeedCheat = (): void => {
+  gameConfig.paddles.speed = 0.4;
+  newGame();
+}
+
+const setThemeAmber = (): void => {
+  fill(255, 176, 0); // AMBER-CRT
+  backgroundLumosity = 0;
+}
+
+const setThemeGreen = (): void => {
+  fill(51, 255, 0); // GREEN-CRT
+  backgroundLumosity = 0;
+}
+
+const setThemeBlack = (): void => {
+  fill(0, 0, 0);
+  backgroundLumosity = 255;
+}
+
+const setThemeWhite = (): void => {
+  fill(255, 255, 255);
+  backgroundLumosity = 0;
+}
+
+let backgroundLumosity = 0;
 
 function setup() {
   createCanvas(400, 400);
   textAlign(CENTER, CENTER);
   textSize(game.config.score.textSize);
-  fill(255, 176, 0); // AMBER-CRT
+
+  createButton("NEW GAME").mouseClicked(newGame);
+  createP();
+  createButton("Ball Size (Tiny)").mouseClicked(setBallSizeTiny);
+  createButton("Ball Size (Normal)").mouseClicked(setBallSizeNormal);
+  createButton("Ball Size (Large)").mouseClicked(setBallSizeLarge);
+  createButton("Ball Size (Comical)").mouseClicked(setBallSizeComical);
+  createP();
+  createButton("Paddle Speed (Normal)").mouseClicked(setPaddleSpeedNormal);
+  createButton("Paddle Speed (Cheat)").mouseClicked(setPaddleSpeedCheat);
+  createP();
+  createButton("Amber Theme").mouseClicked(setThemeAmber);
+  createButton("Green Theme").mouseClicked(setThemeGreen);
+  createButton("Black Theme").mouseClicked(setThemeBlack);
+  createButton("White Theme").mouseClicked(setThemeWhite);
+
 }
 
 function draw() {
-  background(0);
+  background(backgroundLumosity);
   game.process(deltaTime);
 }
 
