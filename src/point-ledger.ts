16258033da4b6e9f11889138ec89f031b60310fe
Nicholas H.R. Sims <nickhrsims@gmail.com>
2025-05-02 21:52:06 -0400

    feat: proper point ledger

---
+++ src/point-ledger.ts
@@ -0,0 +1,68 @@
+interface PointRecordProps {
+  /* Point number. */
+  id: number;
+  /* Duration is only track while ball is actually in motion. */
+  duration: number;
+  /* Which side scored a goal? */
+  scoredBy: 'left' | 'right' | 'none';
+  /* Is game over? */
+  gameOver: boolean;
+};
+
+class PointRecord {
+  props: PointRecordProps;
+
+  private constructor(props: PointRecordProps) {
+    this.props = props;
+  }
+
+  static create(props: PointRecordProps): PointRecord {
+    return new PointRecord(props);
+  }
+}
+
+class LedgerState {
+  pointDuration: number;
+
+  constructor() {
+    this.pointDuration = 0;
+  }
+}
+
+export class PointLedger {
+  private records: PointRecord[];
+  private state: LedgerState;
+
+  private constructor() {
+    this.records = [];
+    this.state = new LedgerState();
+  }
+
+  static create(): PointLedger {
+    return new PointLedger();
+  }
+
+  draw(x: number, y: number): void {
+    const roundDurationSeconds = Math.floor(this.state.pointDuration / 1000);
+    text(str(roundDurationSeconds), x, y);
+  }
+
+  aggregate(): PointRecord[] {
+    return [...this.records];
+  }
+
+  logPoint(scoredBy: 'left' | 'right', gameOver: boolean): void {
+    this.records.push(PointRecord.create({
+      scoredBy,
+      gameOver,
+      id: this.records.length + 1,
+      duration: this.state.pointDuration
+    }));
+  }
+
+
+  logBallMotion(delta: number): void {
+    this.state.pointDuration += delta;
+  }
+}
+
